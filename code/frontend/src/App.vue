bacb7c3a7a050075864589e801814d4be505854b
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-02-16 22:15:17 -0500

    add tailwindcss to frontend

--- code/frontend/src/App.vue
+++ code/frontend/src/App.vue
@@ -8,6 +8,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+      <h1 class="text-3xl font-bold underline">  Hello world!  </h1>
       <HelloWorld msg="You did it!" />
 
       <nav>

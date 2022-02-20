fdd8b4a5a076c958ac0d09f2827614fe3fb76d8d
aTrashPanda <bcarr1911@gmail.com>
2022-02-19 21:53:36 -0500

    Create footer component

---
+++ code/frontend/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="bg-gray-200 text-center lg:text-left">
+    <div class="text-gray-700 text-center p-4">
+      © 2022 Copyright:
+      <a class="text-gray-800" href="https://github.com/cs421sp22-homework/project-team-08-random">
+        JHcourserevU
+      </a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+}
+</script>

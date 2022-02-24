ccc4df2365e5c25c8af13b9e752fe127d5209d96
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-02-24 11:06:16 -0500

    use environ to store secret keys

--- code/backend/course_review/views.py
+++ code/backend/course_review/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here. (The following is copy pasted from https://docs.djangoproject.com/en/3.2/intro/tutorial01/)
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello, world. This is a JHU course review page.")

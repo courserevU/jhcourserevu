b76c02ca7e278ccebd616047c4e2c2862e78792b
Melody Hsu <mhsu13@jhu.edu>
2022-02-25 15:45:32 -0500

    Added some pages to redirect to later, uncommented secret key

--- code/backend/course_review/views.py
+++ code/backend/course_review/views.py
@@ -3,5 +3,20 @@ from django.shortcuts import render
 # Create your views here. (The following is copy pasted from https://docs.djangoproject.com/en/3.2/intro/tutorial01/)
 from django.http import HttpResponse
 
-def index(request):
+def home(request):
     return HttpResponse("Hello, world. This is a JHU course review page.")
+
+def write(request):
+    return HttpResponse("Page for writing a review.")
+
+def read(request):
+    return HttpResponse("Page for reading a review.")
+
+def browse(request):
+    return HttpResponse("Page for viewing all reviews.")
+
+def login(request):
+    return HttpResponse("Page for logging a user in.")
+
+def dashboard(request):
+    return HttpResponse("Page for a moderator to view a dashboard.")
--- code/backend/project/settings.py
+++ code/backend/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-p=^53kc)^8up=2p%zw#irfm7_&%28qucrh*v8zqb1!j^vjey*7'
+SECRET_KEY = 'django-insecure-p=^53kc)^8up=2p%zw#irfm7_&%28qucrh*v8zqb1!j^vjey*7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

ae965c9f35963791d45c8ea9adeab663c96b7544
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-02-27 13:41:01 -0500

    add Melody's urls + views

--- code/backend/course/urls.py
+++ code/backend/course/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('api/write_review/', views.write, name='write'),
+    path('api/read/', views.read, name='read'),
+    path('api/browse/', views.browse, name='browse'),
+    path('api/login/', views.login, name='login'),
+    path('api/dashboard/', views.dashboard, name='dashboard'),
 ]
--- code/backend/course/views.py
+++ code/backend/course/views.py
@@ -4,5 +4,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def home(request):
     return HttpResponse("Hello, world. This is a JHU course review page.")
+
+def write(request):
+    return HttpResponse("Page for writing a review.")
+
+def read(request):
+    return HttpResponse("Page for reading a review.")
+
+def browse(request):
+    return HttpResponse("Page for viewing all reviews.")
+
+def login(request):
+    return HttpResponse("Page for logging a user in.")
+
+def dashboard(request):
+    return HttpResponse("Page for a moderator to view a dashboard.")
+    

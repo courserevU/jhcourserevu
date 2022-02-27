2e83f3760a5181efd6da78f1e52fd4677f196da5
aTrashPanda <bcarr1911@gmail.com>
2022-02-27 13:06:56 -0500

    Routing added to CourseDisplay via buttons

--- code/frontend/src/components/CourseDisplay.vue
+++ code/frontend/src/components/CourseDisplay.vue
@@ -8,18 +8,34 @@
       <h2 class="text-2xl font-extrabold tracking-tight text-gray-900">Courses</h2>
 
       <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        <div v-for="course in filteredCourses" :key="course.id" class="group relative">
-          <div class="mt-4 flex justify-between">
+        <div
+          v-for="course in filteredCourses"
+          :key="course.id"
+          class="group relative py-2 px-3 shadow-md"
+        >
+          <div class="mt-2 flex justify-center">
             <div>
               <h3 class="text-sm text-gray-700">
-                <a :href="course.href">
-                  <span aria-hidden="true" class="absolute inset-0" />
+                <a>
+                  <span aria-hidden="true" class="inset-0" />
                   {{ course.name }}
                 </a>
               </h3>
-              <p class="mt-1 text-sm text-gray-500">{{ course.department }}</p>
+              <p class="mt-2 text-sm text-gray-500">{{ course.department }}</p>
             </div>
             <p class="text-sm font-medium text-gray-900">{{ course.number }}</p>
+          </div>
+          <div class="block inline-flex mt-4 mb-2">
+            <button
+              type="button"
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 mx-1 rounded"
+              @click="goToWriteReview(course)"
+            >Write Review</button>
+            <button
+              type="button"
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 mx-1 rounded"
+              @click="goToReadReviews(course)"
+            >Read Reviews</button>
           </div>
         </div>
       </div>
@@ -30,6 +46,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Search from './Search.vue';
+import Dropdown from './Dropdown.vue';
 
 let courses = [
   {
@@ -52,11 +69,17 @@ export default defineComponent({
       courses,
     }
   },
-  components: { Search },
+  components: { Search, Dropdown },
   methods: {
     updateFilter(e: any) {
       this.query = e;
     },
+    goToWriteReview(course: any) {
+      this.$router.push({ path: "/write", name: "write", params: { course: course } });
+    },
+    goToReadReviews(course: any) {
+      this.$router.push({ name: "read", params: { course: course } });
+    }
   },
   computed: {
     filteredCourses() {

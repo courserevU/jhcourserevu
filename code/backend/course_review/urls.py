c9b46b4d36b8dd2b11531fe852ceabea1ba5255a
Melody Hsu <mhsu13@jhu.edu>
2022-02-28 14:51:20 -0500

    added different api endpoints for pages

--- code/backend/course_review/urls.py
+++ code/backend/course_review/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/api/write_review/', views.write, name='write'),
-    path('/api/read/', views.read, name='read'),
-    path('/api/browse/', views.browse, name='browse'),
-    path('/api/login/', views.login, name='login'),
-    path('/api/dashboard/', views.dashboard, name='dashboard'),
+    path('api/write_review/', views.write, name='write'),
+    path('api/read/', views.read, name='read'),
+    path('api/browse/', views.browse, name='browse'),
+    path('api/login/', views.login, name='login'),
+    path('api/dashboard/', views.dashboard, name='dashboard'),
 ]

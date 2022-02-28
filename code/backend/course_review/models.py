a237e530d0f1d35b13d2027a5c0ceabc070fe425
Melody Hsu <mhsu13@jhu.edu>
2022-02-28 16:37:24 -0500

    added course model skeleton

--- code/backend/course_review/models.py
+++ code/backend/course_review/models.py
@@ -1,3 +1,26 @@
+from socketserver import StreamRequestHandler
+from typing_extensions import Self
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model): 
+    number = models.CharField(max_length = 20)
+    name = models.CharField(max_length = 30)
+    faculty = models.ManyToManyField(models.faculty)
+    semester = models.CharField(max_length = 20)
+    description = models.TextField()
+    pre_reqs = models.ManyToManyField(models.course)
+    # reviews = models.ForeignKey(models.review)
+
+    def edit_course():
+        return
+
+    # def add_review(rev):
+    #     return
+
+    def view_course():
+        return "%s, %s" % (Self.name, Self.number)
+        # return course description summary    
+
+

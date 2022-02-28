339077c4b5b6a2111a44776f01915597d3595269
nhwagle <nhwagle@gmail.com>
2022-02-28 16:41:14 -0500

    review model barebones done

--- code/backend/course/models.py
+++ code/backend/course/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from students.models import Student
+#from students.models import Student
 
 # Create your models here.
 
 class Review(models.Model):
-    def author(self):
+    #def author(self):
         # anonymize
-        return Student.objects.get(user=self.user).name
+    #    return Student.objects.get(user=self.user).name
 
-    COURSES_CHOICES = Student.objects.get(user=self.user).courses
+    #COURSES_CHOICES = Student.objects.get(user=self.user).courses
 
     review_content = models.CharField(max_length=350, default='-')
-    course = models.CharField(max_length=50, choices=COURSES_CHOICES, default='-')
+    #course = models.CharField(max_length=50, choices=COURSES_CHOICES, default='-')
     submit_time = models.DateTimeField(auto_now_add=True)

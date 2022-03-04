34fe0c00932ea98468b64a3359d578428adcb884
Steph <srincon3@jhu.edu>
2022-03-04 16:51:30 -0500

    Nicer REview

--- code/frontend/src/components/ReviewCourse.vue
+++ code/frontend/src/components/ReviewCourse.vue
@@ -28,7 +28,7 @@
 
     <!-- Review Text -->
     <div class="flex space-x-2 justify-left px-20">
-        <div class="form-group mb-3 w-full ">
+        <div class="form-group mb-3 w-full">
             <textarea
                 required
                 class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"

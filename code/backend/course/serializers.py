5adaa1cc97df5d076da49c1bf0085a9a740f7f2a
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-03-05 11:28:13 -0500

    rename course_num in serializer

--- code/backend/course/serializers.py
+++ code/backend/course/serializers.py
@@ -5,7 +5,7 @@ from course.models import Course, LANGUAGE_CHOICES, STYLE_CHOICES
 class CourseSerializer(serializers.ModelSerializer):
     name = serializers.CharField(required=False, allow_blank=True, max_length=255)
     description = serializers.CharField(style={'base_template': 'textarea.html'})
-    number = serializers.CharField(required=False, allow_blank=True, max_length=20)
+    course_num = serializers.CharField(required=False, allow_blank=True, max_length=20)
     num_credits = serializers.FloatField(max_value=15.0, min_value=-1)
     department = serializers.CharField(required=False, allow_blank=True, max_length=255)
     level = serializers.CharField(required=False, allow_blank=True, max_length=500)
@@ -26,7 +26,7 @@ class CourseSerializer(serializers.ModelSerializer):
         """
         Update and return an existing `Snippet` instance, given the validated data.
         """
-        instance.number = validated_data.get('number', instance.number)
+        instance.course_num = validated_data.get('course_num', instance.number)
         instance.name = validated_data.get('name', instance.name)
         instance.faculty = validated_data.get('faculty', instance.faculty)
         instance.semester = validated_data.get('semester', instance.semester)

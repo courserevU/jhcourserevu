8d9508a23ae231fa698a4f4a029f33064cd5932a
aTrashPanda <bcarr1911@gmail.com>
2022-03-08 16:24:25 -0500

    Add dark mode toggle button

--- code/frontend/src/App.vue
+++ code/frontend/src/App.vue
@@ -6,4 +6,16 @@
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+
+localStorage.setItem("user-theme", "dark");
+
+if (
+  localStorage.getItem("user-theme") === "dark" ||
+  (!("user-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
 </script>

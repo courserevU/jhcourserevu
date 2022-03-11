28b86bfc0b884a66e0d00cc3f16eb1f8742ec64e
aTrashPanda <bcarr1911@gmail.com>
2022-03-11 15:40:26 -0500

    Remove Sign Up button from NavBar (future SSO sign in)

--- code/frontend/src/components/NavBar.vue
+++ code/frontend/src/components/NavBar.vue
@@ -84,7 +84,7 @@
         <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
           <!-- Dark mode toggle button; icon is moon during dark mode, sun during light mode -->
           <button
-            class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-100 hover:bg-slate-900 dark:bg-slate-900 dark:hover:bg-gray-100"
+            class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-100 hover:bg-slate-900 dark:bg-slate-900 dark:hover:bg-gray-100"
             @click="switchTheme"
           >
             <MoonIcon
@@ -105,16 +105,10 @@
             />
           </button>
           <button
-            class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900 dark:hover:text-gray-200"
+            class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-blue-500 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-900"
             @click="goToLogin"
           >
             Sign in
-          </button>
-          <button
-            class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-            @click="goToRegister"
-          >
-            Sign up
           </button>
         </div>
       </div>

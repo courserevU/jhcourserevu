32bed75ad9278e42f2fee088d4cbeacd0dc63ddb
stephrg <89754660+stephrg@users.noreply.github.com>
2022-03-16 10:42:29 -0400

    Try to fix errors

--- code/frontend/src/views/HomeView.vue
+++ code/frontend/src/views/HomeView.vue
@@ -9,5 +9,5 @@
 <script setup lang="ts">
 import NavBar from "../components/NavBar.vue";
 import Footer from "../components/Footer.vue";
-import LandingPage from "@/components/LandingPage.vue";
+import LandingPage from "../components/LandingPage.vue";
 </script>

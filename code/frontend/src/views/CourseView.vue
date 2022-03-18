0666a375c81e65d5f502b98a8b1ed67f51d0c6e3
aTrashPanda <bcarr1911@gmail.com>
2022-03-18 19:24:20 -0400

    Tweak to make footer stick to bottom of CourseView

---
+++ code/frontend/src/views/CourseView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <NavBar />
+    <CourseDisplay class="flex-grow" />
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
+import CourseDisplay from "@/components/CourseDisplay.vue";
+</script>

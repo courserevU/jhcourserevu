6ac7c5c3e197554db8a4dc0216b5ad1ff1bb8f13
nhwagle <nhwagle@gmail.com>
2022-03-18 12:43:52 -0400

    removed unnecessary comments

--- code/backend/user/tests.py
+++ code/backend/user/tests.py
@@ -5,12 +5,7 @@ import re
 # Create your tests here.
 
 class UserTestCase(TestCase):
-    """user = models.OneToOneField(User, on_delete=models.deletion.CASCADE)
-    jhed_id = models.CharField(max_length=50, blank=True, null=True, unique=True)
-    jhed_email = models.EmailField(("email address"), unique=True)
-    class_year = models.CharField(max_length=50, blank=True, null=True, unique=True)
-    preferred_name = models.CharField(max_length=30, blank=True, null=True, unique=True)
-    is_admin = models.BooleanField(null=False)"""
+    
     def setUp(self):
         User.objects.create(jhed_id = 'jsmith0',
                             jhed_email = 'jsmith0@jh.edu',

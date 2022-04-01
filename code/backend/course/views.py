daa5f349a15aa91ddafdfdc9e40c4691d33c106e
nhwagle <nhwagle@gmail.com>
2022-04-01 11:26:37 -0400

    added get request to get all courses

--- code/backend/course/views.py
+++ code/backend/course/views.py
@@ -17,6 +17,18 @@ from course.api.serializers import ReviewSerializer
 import json
 from django.forms import model_to_dict
 
+def all_courses(request):
+    courses = (Course.objects.all())
+    courses_list = []
+    for course in courses:
+        course_dict = model_to_dict(course)
+        courses_list.append(course_dict)
+    json_data = {'courses': courses_list}
+
+    #return render(request, 'all_courses.html', json_data)
+    return HttpResponse(json.dumps(json_data), content_type='application/json')
+
+
 def get_reviews_by_course(request, course):
     reviews = (Review.objects.get(course=course))
     review_list = []

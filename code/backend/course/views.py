8f23fe46c7430862dd84b07172f8eee742ccc7f8
nhwagle <nhwagle@gmail.com>
2022-04-01 11:00:29 -0400

    added get request for all reviews by course

--- code/backend/course/views.py
+++ code/backend/course/views.py
@@ -8,7 +8,25 @@
 # from course.serializers import CourseSerializer
 
 # Create your views here. (The following is copy pasted from https://docs.djangoproject.com/en/3.2/intro/tutorial01/)
-# from django.http import HttpResponse
+from django.http import HttpResponse
+from course.models import Course
+from course.models import Review
+from course.api.serializers import CourseSerializer
+from course.api.serializers import ReviewSerializer
+
+import json
+from django.forms import model_to_dict
+
+def get_reviews_by_course(request, course):
+    reviews = (Review.objects.get(course=course))
+    review_list = []
+    for review in reviews:
+        review_dict = model_to_dict(review)
+        review_list.append(review_dict)
+    json_data = {'reviews': review_list}
+    return HttpResponse(json.dumps(json_data), content_type='application/json')
+
+
 
 
 # def home(request):

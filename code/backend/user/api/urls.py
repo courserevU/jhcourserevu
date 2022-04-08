f9bc4608bd575b78475c6d6cc38d89f7bc2fa46f
nhwagle <nhwagle@gmail.com>
2022-04-08 17:14:48 -0400

    import include

--- code/backend/user/api/urls.py
+++ code/backend/user/api/urls.py
@@ -1,6 +1,7 @@
 # from django.conf.urls import url
 from django.urls import path
 from .views import UserList
+from django.conf.urls import include
 
 urlpatterns = [
     path("", UserList.as_view()),

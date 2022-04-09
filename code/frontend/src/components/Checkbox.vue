481d444e1386f0777de9df2bd4dcfadac919ffbc
stephrg <stephie.rincon.godinez@gmail.com>
2022-04-09 18:50:38 -0400

    checkbox version 2

--- code/frontend/src/components/Checkbox.vue
+++ code/frontend/src/components/Checkbox.vue
@@ -1,14 +1,9 @@
 <template>
     <label>
-<<<<<<< HEAD
         <input  type="checkbox"  v-model="model" />
         <span>{{ " " + label }}</span>
         <!-- <input type="checkbox" id="checkbox" v-model="checked">
             <label for="checkbox">{{ checked }}</label> -->
-=======
-        <input type="checkbox" :value="inputValue" v-model="model" />
-        <span class="text-sm text-gray-700 dark:text-gray-300 font-bold">{{ " " + label }}</span>
->>>>>>> e703d3251dd970a69a369f3e4c19040ca1f6b4c9
     </label>
 </template>
 

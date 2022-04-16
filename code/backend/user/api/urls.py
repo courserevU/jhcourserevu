d5d9bcee7b2be7e9adc9350f3fcd6f1604194005
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-04-16 13:32:36 -0400

    remove docker-compose reference to postgres settings

--- code/backend/user/api/urls.py
+++ code/backend/user/api/urls.py
@@ -1,9 +1,8 @@
-# from django.conf.urls import url
-# from django.urls import path
-# from .views import UserDetail
+from django.urls import path
+from .views import UserDetail
 # from django.conf.urls import include
 
-# urlpatterns = [
-    # path("", UserDetail.as_view()),
+urlpatterns = [
+    path("", UserDetail.as_view()),
     # path('microsoft/', include('microsoft_auth.urls', namespace='microsoft'))
-# ]
+]

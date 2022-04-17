b285f8cc8183de265d23a83ce3a4047d05c0baa6
Theodore Xie <txie119@gmail.com>
2022-04-17 16:53:30 -0400

    Set up page with routing, can change details later.

--- code/frontend/src/router/index.ts
+++ code/frontend/src/router/index.ts
@@ -6,6 +6,7 @@ import PageNotFound from "@/views/PageNotFound.vue";
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import CourseView from "@/views/CourseView.vue";
+import MyCoursesView from "@/views/MyCoursesView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,8 +24,12 @@ const router = createRouter({
     {
       path: "/write/:course",
       name: "write",
-
       component: WriteView,
+    },
+    {
+      path: "/my-courses",
+      name: "MyCourses",
+      component: MyCoursesView,
     },
     {
       path: "/read/:course",

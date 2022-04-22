f4e1bbe7684dd977c6526383c0ba885172ee9a9c
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-04-22 11:26:56 -0400

    add environment_plugin for deployed frontend app

--- code/frontend/vite.config.ts
+++ code/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import EnvironmentPlugin from "vite-plugin-environment";
 // import ImportMetaEnvPlugin from "@import-meta-env/unplugin";
 
 // https://vitejs.dev/config/
@@ -10,6 +11,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    EnvironmentPlugin("all"),
     // ImportMetaEnvPlugin.vite({
     //   example: ".env.local",
     // }),

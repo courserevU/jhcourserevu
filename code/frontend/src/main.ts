e3644697c075d89f7386cd30de0a6d9ea293a09f
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-04-22 13:17:22 -0400

    console log vite google client id

--- code/frontend/src/main.ts
+++ code/frontend/src/main.ts
@@ -15,10 +15,12 @@ library.add(fas, far, fab);
 dom.watch();
 
 const app = createApp(App);
+// const client_id = "placeholder";
 // const client_id = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
 
 const gAuthOptions = {
-  clientId: "placeholder",
+  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
   scope: "email",
   prompt: "consent",
   // TODO: set to false once we get data from backend

e5bdd67b970ef3969858e91c4218fada3afb49ad
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-04-22 12:18:54 -0400

    remove old env file, update frontend stage

--- code/frontend/vite.config.ts
+++ code/frontend/vite.config.ts
@@ -5,29 +5,27 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 // import EnvironmentPlugin from "vite-plugin-environment";
 // import path from "path";
-// import ImportMetaEnvPlugin from "@import-meta-env/unplugin";
+import ImportMetaEnvPlugin from "@import-meta-env/unplugin";
 
-const viteEnv = {};
-Object.keys(process.env).forEach((key) => {
-  if (key.startsWith(`VITE_`)) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key];
-  }
-});
+// const viteEnv = {};
+// Object.keys(process.env).forEach((key) => {
+//   if (key.startsWith(`VITE_`)) {
+//     viteEnv[`import.meta.env.${key}`] = process.env[key];
+//   }
+// });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: viteEnv,
+  // define: viteEnv,
   plugins: [
     vue(),
     vueJsx(),
     // EnvironmentPlugin("all"),
-    // ImportMetaEnvPlugin.vite({
-    //   example: ".env.local",
-    // }),
+    // ImportMetaEnvPlugin.vite("all"),
   ],
   resolve: {
     alias: {
-      "@": require("path").resolve(__dirname, "src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });

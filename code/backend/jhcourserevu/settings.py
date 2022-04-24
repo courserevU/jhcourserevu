5f0506e5ade03c976ed7002267620ce39bb40504
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-04-24 16:47:58 -0400

    updated settings

--- code/backend/jhcourserevu/settings.py
+++ code/backend/jhcourserevu/settings.py
@@ -172,8 +172,6 @@ SOCIAL_AUTH_PIPELINE = (
     "user.utils.create_custom_user",
 )
 
-SOCIAL_AUTH_STORAGE = "social_django.models.DjangoStorage"
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -184,7 +182,6 @@ TIME_ZONE = "America/Chicago"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- code/backend/jhcourserevu/urls.py
+++ code/backend/jhcourserevu/urls.py
@@ -1,30 +1,9 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path, re_path
-# from user.api.views import GoogleLogin
 
 urlpatterns = [
     path("admin/", admin.site.urls, name="admin"),
     re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
-    # path("accounts/", include("allauth.urls")),
-    # path("dj-rest-auth/", include("dj_rest_auth.urls")),
-    # path("dj-rest-auth/google/", GoogleLogin.as_view(), name="google_login"),
-    # path("api-auth/", include("rest_framework.urls")),
-    # path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path("course/", include("course.urls"), name="course"),
     path("user/", include("user.urls"), name="user"),
 ]
